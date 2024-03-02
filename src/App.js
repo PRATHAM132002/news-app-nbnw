6b0bff8ddd462ef5775950573551b454f7323ef7
PRATHAM132002 <prathamvpatil13@gmail.com>
2024-03-02 12:13:16 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,73 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/NavBar';
 import Cards from './components/NewsCard';
 
-
-// export default class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       articles: [],
-//       loading: false,
-//       page: 1
-//     };
-//   }
-
-//   async componentDidMount(){
-//     this.loadMoreArticles()
-//   }
-
-//   async componentDidMount() {
-//     let url = "https://newsapi.org/v2/top-headlines?category=business&country=us&apiKey=9fc0b96d69bd42b0a3def6369109d126";
-//     let data = await fetch(url);
-//     let parsedData = await data.json();
-//     this.setState({ articles: parsedData.articles, loading: false });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <Navbar></Navbar>
-
-
-//         <div className="min-h-screen flex items-center justify-center bg-gray-200 dark:bg-gray-900">
-//           <div className="">
-
-//             <h3 className="text-center text-3xl font-bold py-10 dark:text-white">Top Headlines</h3>
-
-//             <div className="grid grid-cols-2 gap-4 md:gap-4">
-//               {this.state.articles.map((element) => {
-
-
-
-//                 return <Cards title={element.title.substring(0, 50)} description={element.description?.substring(0, 100) || 'No description provided.'} urlToImage={element.urlToImage} url={element.url}></Cards>
-
-//               })}
-
-//             </div>
-
-//             <div className="flex justify-center mt-5">
-//               <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={this.loadMoreArticles}>More Stories</button>
-//             </div>
-
-//           </div>
-//         </div>
-
-//       </div>
-//     )
-//   }
-// }
-
-
 export default class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
+      darkMode: false,
       articles: [],
       loading: false,
       page: 1
     };
+    this.toggleDarkMode = this.toggleDarkMode.bind(this);
     this.loadMoreArticles = this.loadMoreArticles.bind(this);
   }
 
@@ -83,36 +28,47 @@ export default class App extends Component {
     this.setState({ articles: this.state.articles.concat(parsedData.articles), loading: false, page: this.state.page + 1 });
   }
 
+  toggleDarkMode() {
+    this.setState({ darkMode: !this.state.darkMode });
+  }
+
   render() {
+    const { darkMode, articles } = this.state;
     return (
-      <div>
-        <Navbar></Navbar>
-
-
+      <div className={`${darkMode && "dark"}`}>
+        <Navbar />
         <div className="min-h-screen flex items-center justify-center bg-gray-200 dark:bg-gray-900">
           <div className="">
-
             <h3 className="text-center text-3xl font-bold py-10 dark:text-white">Top Headlines</h3>
-
             <div className="grid grid-cols-2 gap-4 md:gap-4">
-              {this.state.articles.map((element) => {
-
-
-
-                return <Cards title={element.title.substring(0, 50)} description={element.description?.substring(0, 100) || 'No description provided.'} urlToImage={element.urlToImage} url={element.url}></Cards>
-
-              })}
-
+              {articles.map((element) => (
+                <Cards
+                  key={element.title} 
+                  title={element.title.substring(0, 50)}
+                  description={element.description?.substring(0, 100) || 'No description provided.'}
+                  urlToImage={element.urlToImage}
+                  url={element.url}
+                />
+              ))}
             </div>
-
             <div className="flex justify-center mt-5">
-              <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={this.loadMoreArticles}>More Stories</button>
+              <button
+                type="button"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                onClick={this.loadMoreArticles}
+              >
+                More Stories
+              </button>
             </div>
-
           </div>
         </div>
-
+        <button
+          className="fixed w-10 h-10 bottom-10 right-10 bg-neutral-900 dark:bg-white rounded-full text-white dark:text-black font-semibold"
+          onClick={this.toggleDarkMode}
+        >
+          {darkMode ? "LHT" : "DRK"}
+        </button>
       </div>
-    )
+    );
   }
 }
--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,9 +1,12 @@
-
-
 import React, { Component } from 'react'
+
+import altImage from '../assets/alt_image.png';
 
 export default class NewsCard extends Component {
 
+    handleImageError = (event) => {
+        event.target.src = {altImage};
+    }
 
     render() {
         let { title, description, urlToImage, url } = this.props;
@@ -11,7 +14,7 @@ export default class NewsCard extends Component {
             <div>
 
                 <a target="_blank" rel="noreferrer" href={url} className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow p-4 md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                    <img className="object-fill rounded-t-lg h-96 md:h-40 md:w-48 md:rounded-none md:rounded-s-lg" style={{objectFit: 'cover'}} src={urlToImage} alt="  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|>_<|  " />
+                    <img className="object-fill rounded-t-lg h-96 md:h-40 md:w-48 md:rounded-none md:rounded-s-lg" style={{objectFit: 'cover'}} src={urlToImage} alt="  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;|>_<|  " onError={this.handleImageError} />
                     <div className="flex flex-col justify-between p-4 leading-normal">
                         <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">{title}...</h5>
                         <p className="mb-3 font-normal text-sm text-gray-700 dark:text-gray-400">{description}... <br />Read More</p>
@@ -22,6 +25,3 @@ export default class NewsCard extends Component {
         )
     }
 }
-
-
-
